6361c26c89d03df2aec800790d3b9715409db912
Cristian <cristian@rotrer.com>
2014-04-11 22:07:39 -0300

    wpconfig

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,3 +3,7 @@ set :stage, :staging
 set :deploy_to, '/home/rotrer/stage/wp-capistrano'
 set :linked_files, %w{config-staging.php}
 set :branch, "master"
+
+set :db_database, "wp-capistrano"
+set :db_username, "wp-capistrano"
+set :db_password, "capistrano"
